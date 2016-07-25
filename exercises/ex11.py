2aba237a8316a570daa00ebcd4338ec5262e8b47
Andrew James <andrew.james@my.com>
2016-07-25 18:37:51 +0300

    commit 5

--- exercises/ex11.py
+++ exercises/ex11.py
@@ -5,4 +5,4 @@ height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heaver." % (age, height, weight)
+print "So, you're %r old, %r tall and %r kg's, fatty." % (age, height, weight)
