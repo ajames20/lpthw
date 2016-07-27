c5949175cd7a50fe6f2bde52f2d6a27a1759e113
Andrew James <andrew.james@my.com>
2016-07-27 14:29:13 +0300

    commit 8

---
+++ exercises/sd19.py
@@ -0,0 +1,20 @@
+def amount_of_shoes(shoes, shoe_laces):
+    print "You have %d shoes!" % shoes
+    print "You have %d laces on shoes!" % shoe_laces
+    print "Man that's enough for a rope!"
+    print "Get a rope now.\n"
+
+print "Thats a lot of shoes!"
+amount_of_shoes(20, 40)
+
+print "This is math for my script"
+amount_of_shoes(2 * 20, 2 * 40)
+
+print "This is more math"
+amount_of_shoes(40 * 2, 80 * 2)
+
+shoes = 100
+shoe_laces = (shoes * 2)
+
+print "This accessing variables"
+amount_of_shoes(shoes * 2, shoe_laces * 2)
